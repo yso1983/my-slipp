565b991d5959f92c71fe7bf967531eb9786f4db0
yongsoo <yso1983@gmail.com>
2019-03-05 21:59:01 +0900

    question & war deploy

--- src/main/java/net/slipp/domain/Question.java
+++ src/main/java/net/slipp/domain/Question.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
@@ -14,48 +16,72 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 public class Question extends AbstractEntity {
-	@ManyToOne
-	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
-	@JsonProperty
-	private User writer;
+	@Id
+	@GeneratedValue
+	private Long id;
 	
-	@JsonProperty
+	private String writer;
+	
 	private String title;
 	
-	@Lob
-	@JsonProperty
-	private String contents;
-	
-	@JsonProperty
-	private Integer countOfAnswer = 0;
-	
-	@OneToMany(mappedBy="question")
-	@OrderBy("id DESC")
-	private List<Answer> answers;
-	
+	private String Contents;
+
 	public Question() {}
-	
-	public Question(User writer, String title, String contents) {
+
+	public Question(String writer, String title, String contents) {
+		super();
 		this.writer = writer;
 		this.title = title;
-		this.contents = contents;
-	}
-	
-	public void update(String title, String contents) {
-		this.title = title;
-		this.contents = contents;
+		Contents = contents;
 	}
 
-	public boolean isSameWriter(User loginUser) {
-		System.out.println("writer : " + writer);
-		return this.writer.equals(loginUser);
+	public Long getId() { 
+		return id;
 	}
 	
-	public void addAnswer() {
-		this.countOfAnswer += 1;
-	}
 	
-	public void deleteAnswer() {
-		this.countOfAnswer -= 1;
-	}
+//	@ManyToOne
+//	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
+//	@JsonProperty
+//	private User writer;
+//	
+//	@JsonProperty
+//	private String title;
+//	
+//	@Lob
+//	@JsonProperty
+//	private String contents;
+//	
+//	@JsonProperty
+//	private Integer countOfAnswer = 0; 
+//	
+//	@OneToMany(mappedBy="question")
+//	@OrderBy("id DESC")
+//	private List<Answer> answers;
+//	
+//	public Question() {}
+//	
+//	public Question(User writer, String title, String contents) {
+//		this.writer = writer;
+//		this.title = title;
+//		this.contents = contents;
+//	}
+//	
+//	public void update(String title, String contents) {
+//		this.title = title;
+//		this.contents = contents;
+//	}
+//
+//	public boolean isSameWriter(User loginUser) {
+//		System.out.println("writer : " + writer);
+//		return this.writer.equals(loginUser);
+//	}
+//	
+//	public void addAnswer() {
+//		this.countOfAnswer += 1;
+//	}
+//	
+//	public void deleteAnswer() {
+//		this.countOfAnswer -= 1;
+//	}
 }
